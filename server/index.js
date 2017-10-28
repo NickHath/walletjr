9ba5ca5bec6527bfd30406030ee14c127fbd8d52
Nick Hathaway <Nicholashathaway@gmail.com>
2017-10-28 02:02:56 -0600

    deleted comments for old http requests

--- server/index.js
+++ server/index.js
@@ -52,17 +52,19 @@ passport.use( new Auth0Strategy({
         }
     })
 }))
+
 passport.serializeUser( function(id, done){
     done(null, id)
 })
 passport.deserializeUser( function(id, done){
-   app.get('db').find_session_user([id]).then((user) =>{
+  app.get('db').find_session_user([id]).then((user) =>{
     done(null, user[0]);
     }
-)
-    
+  )
 })
+
 app.get('/auth', passport.authenticate('auth0'))
+
 app.get('/auth/callback', passport.authenticate('auth0', {
     successRedirect: 'http://localhost:3000/Dashboard',
     failureRedirect:'/auth'
@@ -76,95 +78,11 @@ app.get("/auth/me", (req, res)=>{
         return res.status(401).send('need to log in!')
     }
 })
+
 app.get('/auth/logout', (req, res) => {
     req.logOut();
     res.redirect('http://localhost:3000/')
 })
-// var instance = axios.create({
-//   cert: './galileo16.pem'
-// })
 
-// const baseURL = 'https://sandbox-api.gpsrv.com/intserv/4.0/';
-// let data = {
-//   params: {
-//     'apiLogin': process.env.API_LOGIN,
-//     'apiTransKey': process.env.API_TRANS_KEY,
-//     'providerId': process.env.API_PROVIDER_ID,
-//     'prodId': process.env.API_PRODID
-//   }
-// };
-
-// const https = require('https');
-// const fs = require('fs')
-
-// var agent = new https.Agent({
-//   ca: fs.readFileSync('./galileo16.pem')
-// });
-// console.log(fs.readFileSync('./galileo16.pem').toString())
-
-
-// axios.post(baseURL + 'ping', { agent })
-//      .then(res => console.log(res));
-
-
-
-// app.get(`/api/test`, (req, res) => {
-//   let createAcctPayload = Object.assign({}, data, {'transactionId' : '68604-random-string-74629'})
-//   axios.post(baseURL + 'createAccount', createAcctPayload)
-//        .then(account => {
-//          res.status(200).send(account);
-//        })
-// });
-
-
-// let data = {
-//   'apiLogin': process.env.API_LOGIN,
-//   'apiTransKey': process.env.API_TRANS_KEY,
-//   'providerId': process.env.API_PROVIDER_ID,
-//   'prodId': process.env.API_PRODID
-// };
-
-// var fs = require('fs'); 
-// var https = require('https'); 
-// var options = { 
-//     hostname: 'sandbox-api.gpsrv.com', 
-//     port: 443, 
-//     path: '/intserv/4.0/ping', 
-//     method: 'POST', 
-//     ca: fs.readFileSync('galileo16.pem') 
-// }; 
-// var req = https.request(options, function(res) { 
-//     res.on('data', function(data) { 
-//         process.stdout.write(data); 
-//     }); 
-// }); 
-// req.end();
-
-// massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
-
-
-// var https = require('https'),                  // Module for https
-// fs =    require('fs');                     // Required to read certs and keys
-
-// var options = { 
-//     hostname: 'sandbox-api.gpsrv.com', 
-//     port: 443, 
-//     path: '/intserv/4.0/ping', 
-//     method: 'POST', 
-//     ca: fs.readFileSync('galileo16.pem') 
-// }; 
-
-// callback = function(response) {
-// var str = '';    
-// response.on('data', function (chunk) {
-// str += chunk;
-// });
-
-// response.on('end', function () {
-// console.log(str);
-// });
-// }
-
-// https.request(options, callback).end();
 const PORT = 4200
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
