f76c272f2fb7f584467ae2cf7d8b17e798ccd0f3
Tyler Cook <cooktylerjames@gmail.com>
2017-10-27 21:09:47 -0600

    dash css changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -17,7 +17,7 @@ export default class Dashboard extends Component {
         </div>
         <div className = "dash_body">
             
-            <div>
+            <div></div>
                
                 </div>
             <div>
