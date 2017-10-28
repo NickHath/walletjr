eceb33fab61ffaaf9ebe33b4a36042ddb4460940
tingey21 <alextingey1@gmail.com>
2017-10-27 19:09:00 -0600

    Landing page started

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+  render() {
+
+    users.map(user =>{
+    return(
+        <div> user.name</div>
+    ))
+    return (
+      <div className = "LandingPage">
+        <div className = "header">
+            <div>Name</div>
+            <div>Header</div>
+        </div>
+        <div className = "body">
+            <div><img src="" alt=""/></div>
+            <div>
+                <h1>
+                    WalletJr.com
+                </h1>
+                </div>
+            <div>
+                <h3>Description</h3>
+            </div>
+        </div>
+
+      </div>
+    )
+  }
+}
